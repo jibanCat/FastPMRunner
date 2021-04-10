c987402367c9d488e6be884d352b70698a1ae112
jibancat <mho026@ucr.edu>
2021-04-10 08:39:04 -0700

    runnable now

--- FastPMRunner/simulationic.py
+++ FastPMRunner/simulationic.py
@@ -1,10 +1,13 @@
 """
 A class to automate the fastpm simulation
 """
+from typing import Tuple
+
 import os
 import subprocess
 
 from .make_pklin import save_powerspec
+from .lua_template import simple_lua_string
 
 class SimulationICs(object):
     """
@@ -33,37 +36,47 @@ class SimulationICs(object):
     ns         - Scalar spectral index
     timesteps  - number of time steps for the simulation
     """
-    def __init__(self,
-            outdir: str = "nbodykit",   box: int = 384,  npart: int = 128,
-            seed :         int   = 100,          redshift: float = 99,
-            redend:        float = 0,            omega0:   float = 0.288, 
-            omegab:        float = 0.0472,       hubble:   float = 0.7,
-            scalar_amp:    float = 2.427e-9,     ns:       float = 0.97,
-            fastpm_bin:    str = "fastpm",       timesteps: float = 10,
-            python:        str = "python",
-            cores:         int = 4) -> None:
+
+    def __init__(
+        self,
+        outdir: str = "nbodykit",
+        param_file: str = "param.lua",
+        box: int = 384,
+        npart: int = 128,
+        seed: int = 100,
+        redshift: float = 99,
+        redend: float = 0,
+        omega0: float = 0.288,
+        omegab: float = 0.0472,
+        hubble: float = 0.7,
+        scalar_amp: float = 2.427e-9,
+        ns: float = 0.97,
+        fastpm_bin: str = "fastpm",
+        timesteps: float = 10,
+        cores: int = 4,
+    ) -> None:
 
         self.outdir = outdir
+        self.param_file = param_file
 
-        #Check that input is reasonable and set parameters
-        #In Mpc/h
-        assert box  < 20000
-        self.box      = box
+        # Check that input is reasonable and set parameters
+        # In Mpc/h
+        assert box < 20000
+        self.box = box
 
-        #Cube root
+        # Cube root
         assert npart > 1 and npart < 16000
-        self.npart    = int(npart)
+        self.npart = int(npart)
 
-        #Physically reasonable
+        # Physically reasonable
         assert omega0 <= 1 and omega0 > 0
-        self.omega0   = omega0
+        self.omega0 = omega0
 
         assert omegab > 0 and omegab < 1
-        self.omegab   = omegab
+        self.omegab = omegab
 
         assert redshift > 1 and redshift < 1100
         self.redshift = redshift
-
 
         assert redend >= 0 and redend < 1100
         self.redend = redend
@@ -78,14 +91,17 @@ class SimulationICs(object):
 
         assert ns > 0 and ns < 2
         self.ns = ns
-        
+
         self.seed = seed
 
         # the folder to store simulation outputs
-        if ~ os.path.exists(self.outdir):
+        if not os.path.exists(self.outdir):
             os.mkdir(self.outdir)
 
-    def make_pklin(self, outfile: str = "powerspec.txt") -> None:
+        self.fastpm_bin = fastpm_bin
+        self.cores = cores
+
+    def make_pklin(self, outfile: str = "my_pk_linear.txt") -> None:
         """
         Make linear power spectrum and save as a file
         """
@@ -93,16 +109,67 @@ class SimulationICs(object):
         self.linear_file = os.path.join(self.outdir, outfile)
 
         save_powerspec(
-            omega0 = self.omega0,
-            omegab = self.omegab,
-            hubble = self.hubble,
-            scalar_amp = self.scalar_amp,
-            ns = self.ns,
+            omega0=self.omega0,
+            omegab=self.omegab,
+            hubble=self.hubble,
+            scalar_amp=self.scalar_amp,
+            ns=self.ns,
             outfile=self.linear_file,
         )
 
-    def make_simulation(self) -> None:
+    def make_simulation(
+        self,
+        write_runpb_snapshot: bool = False,
+        write_snapshot: bool = False,
+        write_fof: bool = False,
+    ) -> Tuple[str, str]:
         """
-        Generate .lue input parameter file for fastpm simulation
+        Generate .lua input parameter file for fastpm simulation
         """
+        if "linear_file" not in dir(self):
+            self.make_pklin()
 
+        # start/end time in scale factor
+        time_start = 1 / (1 + self.redshift)
+        time_end = 1 / (1 + self.redend)
+
+        write_powerspectrum = os.path.join(self.outdir, "powerspec")
+
+        lua_string = simple_lua_string(
+            box=self.box,
+            npart=self.npart,
+            seed=self.seed,
+            omega0=self.omega0,
+            omegab=self.omegab,
+            hubble=self.hubble,
+            scalar_amp=self.scalar_amp,
+            ns=self.ns,
+            time_start=time_start,
+            time_end=time_end,
+            timesteps=self.timesteps,
+            read_powerspectrum=self.linear_file,
+            write_powerspectrum=write_powerspectrum,
+            write_runpb_snapshot=write_runpb_snapshot,
+            write_snapshot=write_snapshot,
+            write_fof=write_fof,
+        )
+
+        with open(os.path.join(self.outdir, self.param_file), "w") as f:
+            f.write(lua_string)
+
+        # run FastPM
+        bash_command = "mpirun -n {cores} {fastpm_bin} {param_file}".format(
+            cores=self.cores,
+            fastpm_bin=self.fastpm_bin,
+            param_file=os.path.join(self.outdir, self.param_file),
+        )
+        print(bash_command.split())
+
+        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+        # write the output
+        with open(os.path.join(self.outdir, "message.out"), "w") as f:
+            f.write(output.decode('utf-8'))
+
+        return output, error
