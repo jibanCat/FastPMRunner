046c6a75db5b40d57f3f62a14691cc6db76305c2
Simeon Bird <sbird@ucr.edu>
2021-04-12 20:40:36 -0700

    Boil down to a semi-comprehensible monopole

--- bao_fs_ngc_z1_marg/__init__.py
+++ bao_fs_ngc_z1_marg/__init__.py
@@ -12,6 +12,7 @@ as the point of this is to explore numerical approximations.
 I have also simplified this by:
 1. Removing the Alcock-Paczynski test which is a bit complicated to implement, and does not dominate the constraints.
 2. Removing the shot noise power which is close to zero
+3. Only compute the monopole terms
 """
 
 import os
@@ -23,6 +24,7 @@ from scipy import interpolate
 from scipy.integrate import quad
 import scipy.integrate as integrate
 from scipy import special
+from FastPMRunner.simulationic import SimulationICs
 
 class Likelihood:
     """Likelihood function for the BOSS power spectrum."""
@@ -66,7 +68,7 @@ class Likelihood:
             self.Pk2[i] = float(line.split()[2])
         datafile.close()
 
-        ## LOAD OTHER USEFUL FUNCTIONS
+        ## Window function: only W0 is used.
         self.Nmax=128
         self.W0 = np.zeros((self.Nmax,1))
         self.W2 = np.zeros((self.Nmax,1))
@@ -107,12 +109,10 @@ class Likelihood:
             r_pow = r**(-3.-1.*nu)
             sin_nu = np.sin(pi*nu/2.)
             J0 = -1.*sin_nu*r_pow*gam/(2.*pi**2.)
-            J2 = -1.*r_pow*(3.+nu)*gam*sin_nu/(nu*2.*pi**2.)
-            return J0,J2
+            return J0
 
-        j0,j2 = J_func(rtab.reshape(-1,1),self.etam.reshape(1,-1))
+        j0 = J_func(rtab.reshape(-1,1),self.etam.reshape(1,-1))
         self.J0_arr = j0[:,:,np.newaxis]
-        self.J2_arr = j2[:,:,np.newaxis]
 
         self.etamR = bR + 2*1j*pi*(jsNm)/self.Nmax/Delta_r
 
@@ -121,99 +121,115 @@ class Likelihood:
             k_pow = k**(-3.-1.*nu)
             sin_nu = np.sin(pi*nu/2.)
             J0k = -1.*k_pow*gam*sin_nu*(4.*pi)
-            J2k = -1.*k_pow*(3.+nu)*gam*sin_nu*4.*pi/nu
-            return J0k,J2k
+            return J0k
 
-        j0k,j2k = Jk_func(self.kbins3.reshape(-1,1),self.etamR.reshape(1,-1))
+        j0k = Jk_func(self.kbins3.reshape(-1,1),self.etamR.reshape(1,-1))
         self.J0k_arr = j0k[:,:,np.newaxis]
-        self.J2k_arr = j2k[:,:,np.newaxis]
 
         #Precompute inverse covariance matrix
         self.invcov = np.linalg.inv(self.cov)
 
-    def loglkl(self, cosmo, data):
-        """Compute the log-likelihood of the model, given the data and covariance"""
+        # Compute window response matrix
+        self.response_matrix = np.zeros((self.ksize,self.Nmax))
+        for i in range(self.Nmax):
+            tmp_resp0 = self.window_response(i)
+            self.response_matrix[:,i] = tmp_resp0[0]
 
-        ## First load in cosmological and explicitly-sampled nuisance parameters at this MCMC step
-        h = cosmo.h()
+        # Now convolve with window function
+        self.invcovW = np.matmul(self.response_matrix.T,self.invcov)
+        self.invcovWW = np.matmul(self.response_matrix.T,np.matmul(self.invcov,self.response_matrix))
 
-        norm = 1.
+    def window_response(self,k_index):
+        """Window function. This is from 1607.03150 and includes only the monopole.
+        Accurate for k > 0.0015."""
+        Nmax = self.Nmax
         k0 = self.k0
 
-        # Now load in (mean, sigma) for nuisance parameters that are analytically marginalized over
-        Nmarg = 0 # number of parameters to marginalize over analytically
+        Pdiscrin0 = np.zeros(Nmax)
+        Pdiscrin0[k_index] = 1
 
-        ## COMPUTE SPECTRA
-        # Run CLASS-PT to get all components
-        all_theory = cosmo.get_pk_mult(self.kbins3*h,self.z, self.Nmax)
+        cm0 = np.fft.fft(Pdiscrin0)/ Nmax
+        cmsym0 = np.zeros(Nmax+1,dtype=np.complex_)
 
-        # Generate the theory model from FastPMRunner
-        theory2 = norm**2.*all_theory[18] +norm**4.*all_theory[24]
-        theory0 = norm**2.*all_theory[15] +norm**4.*all_theory[21]
-
-        # Now do a Fourier-transform to include the window function
-        factor = (exp(-1.*(self.kbins3*h/2.)**4.)*self.tmp_factor)[:,np.newaxis]
-        Pdiscrin0 = theory0*factor
-        Pdiscrin2 = theory2*factor
-
-        cm0 = np.fft.fft(Pdiscrin0,axis=0)/ self.Nmax
-        cm2 = np.fft.fft(Pdiscrin2,axis=0)/ self.Nmax
-        cmsym0 = np.zeros((self.Nmax+1,Nmarg+1),dtype=np.complex_)
-        cmsym2 = np.zeros((self.Nmax+1,Nmarg+1),dtype=np.complex_)
-
-        all_i = np.arange(self.Nmax+1)
-        f = (all_i+2-self.Nmax//2) < 1
-        k0t1 = (k0**(-self.etam[f]))[:,np.newaxis]
-        k0t2 = (k0**(-self.etam[~f]))[:,np.newaxis]
-        cmsym0[f] = k0t1*np.conjugate(cm0[-all_i[f]+self.Nmax//2])
-        cmsym2[f] = k0t1*np.conjugate(cm2[-all_i[f]+self.Nmax//2])
-        cmsym0[~f] = k0t2*cm0[all_i[~f]-self.Nmax//2]
-        cmsym2[~f] = k0t2*cm2[all_i[~f]-self.Nmax//2]
+        all_i = np.arange(Nmax+1)
+        f = (all_i+2-Nmax//2) < 1
+        cmsym0[f] = k0**(-self.etam[f])*np.conjugate(cm0[-all_i[f]+Nmax//2])
+        cmsym0[~f] = k0**(-self.etam[~f])*cm0[all_i[~f]-Nmax//2]
 
         cmsym0[-1] = cmsym0[-1] / 2
         cmsym0[0] = cmsym0[0] / 2
-        cmsym2[-1] = cmsym2[-1] / 2
-        cmsym2[0] = cmsym2[0] / 2
 
-        xi0 = np.real(cmsym0[np.newaxis,:,:]*self.J0_arr).sum(axis=1)
-        xi2 = np.real(cmsym2[np.newaxis,:,:]*self.J2_arr).sum(axis=1)
-        Xidiscrin0 = (xi0*self.W0 + 0.2*xi2*self.W2)*self.tmp_factor2
-        Xidiscrin2 = (xi0*self.W2 + xi2*(self.W0 + 2.*(self.W2+self.W4)/7.))*self.tmp_factor2
+        xi0 = np.real(cmsym0*self.J0_arr).sum(axis=1)
+        Xidiscrin0 = (xi0*self.W0)*self.tmp_factor2
 
-        cmr0 = np.fft.fft(Xidiscrin0,axis=0)/ self.Nmax
-        cmr2 = np.fft.fft(Xidiscrin2,axis=0)/ self.Nmax
+        cmr0 = np.fft.fft(Xidiscrin0)/ Nmax
 
-        cmsymr0 = np.zeros((self.Nmax+1,Nmarg+1),dtype=np.complex_)
-        cmsymr2 = np.zeros((self.Nmax+1,Nmarg+1),dtype=np.complex_)
+        cmsymr0 = np.zeros(Nmax+1,dtype=np.complex_)
 
-        arr_i = np.arange(self.Nmax+1)
-        f = (arr_i+2-self.Nmax//2)<1
-        r0t1 = self.rmin**(-self.etamR[f])[:,np.newaxis]
-        r0t2 = self.rmin**(-self.etamR[~f])[:,np.newaxis]
-        cmsymr0[f] = r0t1*np.conjugate(cmr0[-arr_i[f] + self.Nmax//2])
-        cmsymr2[f] = r0t1*np.conjugate(cmr2[-arr_i[f] + self.Nmax//2])
-        cmsymr0[~f] = r0t2*cmr0[arr_i[~f] - self.Nmax//2]
-        cmsymr2[~f] = r0t2*cmr2[arr_i[~f] - self.Nmax//2]
+        arr_i = np.arange(Nmax+1)
+        f = (arr_i+2-Nmax//2)<1
+
+        cmsymr0[f] = self.rmin**(-self.etamR[f])*np.conjugate(cmr0[-arr_i[f] + Nmax//2])
+        cmsymr0[~f] = self.rmin**(-self.etamR[~f])* cmr0[arr_i[~f] - Nmax//2]
 
         cmsymr0[-1] = cmsymr0[-1] / 2
         cmsymr0[0] = cmsymr0[0] / 2
-        cmsymr2[-1] = cmsymr2[-1] / 2
-        cmsymr2[0] = cmsymr2[0] / 2
 
-        P0t = np.real(cmsymr0[np.newaxis,:,:]*self.J0k_arr).sum(axis=1)
-        P2t = np.real(cmsymr2[np.newaxis,:,:]*self.J2k_arr).sum(axis=1)
+        P0t = np.real(cmsymr0*self.J0k_arr).sum(axis=1)
+        P0int = interpolate.InterpolatedUnivariateSpline(self.kbins3,P0t)(self.k)
+        return P0int
 
-        P0int = np.asarray([interpolate.InterpolatedUnivariateSpline(self.kbins3,P0t[:,i])(self.k) for i in range(Nmarg+1)]).T
-        P2int = np.asarray([interpolate.InterpolatedUnivariateSpline(self.kbins3,P2t[:,i])(self.k) for i in range(Nmarg+1)]).T
+    def loglkl(self, params, box=384, npart=128, timesteps=10):
+        """Compute the log-likelihood of the model, given the data and covariance loaded in __init__.
+        The cosmology and model parameters are put in the params list.
+        The fastpm accuracy parameters box, npart and timesteps are kw args.
+        Model parameters:
+        0 - h the hubble parameter
+        1 - Omega_0 the total matter density
+        2 - scalar_amp the primordial amplitude of fluctuations at k = 0.05 Mpc^{-1}
+        3 - The linear bias, a variable which encodes how much galaxies differ from cold dark matter.
+        """
+        #Cosmology parameters
+        hubble = params[0]
+        omega0 = params[1]
+        scalar_amp = params[2]
+        #ns is not measured by BOSS very well, so fixed
+        #ns = params[3]
+        linear_bias = params[3]
 
-        # COMPUTE CHI^2 OF MODEL
-        chi2 = 0.
+        ## COMPUTE SPECTRA
+        # Run fastPM for power spectrum with cosmological parameters.
+        sim = SimulationICs(hubble = hubble,
+                            omega0 = omega0,
+                            scalar_amp = scalar_amp,
+                            ns = 0.9649,
+                            fastpm_bin = "fastpm",
+                            box = box,
+                            npart = npart,
+                            timesteps = timesteps,
+                            redend = self.z,
+                            )
+        sim.make_simulation()
+        powerspec = sim.powerspecs[-1]
+        kk = sim.kk[-1]
+        #Galaxy power spectrum with multipoles, for reference.
+        #Powergal = lambda mu, l: (linear_bias + ff * mu**2)**2 * sim.powerspecs[-1]
+        #For higher moments we should do a multipole integral. Here just use l = 0 for simplicity.
+        #Neglect the redshift space distortions because they are degenerate with linear bias
+        #for the monopole.
+        powergal = linear_bias**2 * powerspec
+        powgalint = interpolate.InterpolatedUnivariateSpline(kk,powergal)
 
-        # Compute [model - data] for P(k) multipoles
-        x1 = np.hstack([P0int[:,0]-self.Pk0,P2int[:,0]-self.Pk2])
+        # Now do a Fourier-transform to include the window function
+        factor = np.exp(-1.*(self.kbins3*hubble/2.)**4.)*self.tmp_factor
+        factint = interpolate.InterpolatedUnivariateSpline(self.kbins3*hubble, factor)
+        Pdisc = powgalint(self.k)*factint(self.k)
 
-        # Compute chi2
-        chi2 = np.inner(x1,np.inner(self.invcov,x1))
+        # Now compute chi^2: this is:
+        #(Pth W - Pexp)^T C^-1 (Pth W - Pexp)
+        chi2 = np.inner(Pdisc[:,0],np.inner(self.invcovWW,Pdisc[:,0]))
+        chi2 += np.inner(self.Pk0,np.inner(self.invcov,self.Pk0))
+        chi2 += -2.*np.inner(Pdisc[:,0],np.inner(self.invcovW,self.Pk0))
 
         # Compute log-likelihood
         loglkl = -0.5 * chi2
